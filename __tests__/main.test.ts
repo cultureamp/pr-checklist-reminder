bf7d6eb7ae004566b4df4e34064fb301f1a347d5
Michael Bylstra <mbylstra@gmail.com>
2020-07-16 10:29:55 +1000

    make eslint more lenient

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -6,151 +6,151 @@ import {
   getGithubStatusSpecs,
   findChecklistItem,
   makeWhitelistFromArgs
-} from "../src/main";
+} from '../src/main'
 
-describe("parseMarkdownChecklistItems", () => {
-  it("correctly parses markdown for checklist items", async () => {
+describe('parseMarkdownChecklistItems', () => {
+  it('correctly parses markdown for checklist items', async () => {
     const markdown = [
-      "- [ ] unchecked checklist item 1",
-      "- [x] checked checklist item 2",
-      "not a checklist item"
-    ].join("\n");
+      '- [ ] unchecked checklist item 1',
+      '- [x] checked checklist item 2',
+      'not a checklist item'
+    ].join('\n')
     expect(parseMarkdownChecklistItems(markdown)).toEqual([
       {
-        description: "unchecked checklist item 1",
+        description: 'unchecked checklist item 1',
         checked: false
       },
       {
-        description: "checked checklist item 2",
+        description: 'checked checklist item 2',
         checked: true
       }
-    ]);
-  });
-});
+    ])
+  })
+})
 
-describe("parseMarkdownChecklistItem", () => {
-  it("correctly parses an unchecked item", async () => {
-    const markdown = "- [ ] unchecked checklist item 1";
+describe('parseMarkdownChecklistItem', () => {
+  it('correctly parses an unchecked item', async () => {
+    const markdown = '- [ ] unchecked checklist item 1'
     expect(parseMarkdownChecklistItem(markdown)).toMatchObject({
-      description: "unchecked checklist item 1",
+      description: 'unchecked checklist item 1',
       checked: false
-    });
-  });
-  it("correctly parses a checked item", async () => {
-    const markdown = "- [x] checked checklist item 1";
+    })
+  })
+  it('correctly parses a checked item', async () => {
+    const markdown = '- [x] checked checklist item 1'
     expect(parseMarkdownChecklistItem(markdown)).toMatchObject({
-      description: "checked checklist item 1",
+      description: 'checked checklist item 1',
       checked: true
-    });
-  });
-  it("returns null if not a checklist item", async () => {
-    const markdown = "-[x] not actually a checklist item";
-    expect(parseMarkdownChecklistItem(markdown)).toBeNull();
-  });
-});
+    })
+  })
+  it('returns null if not a checklist item', async () => {
+    const markdown = '-[x] not actually a checklist item'
+    expect(parseMarkdownChecklistItem(markdown)).toBeNull()
+  })
+})
 
-describe("truncateDescriptionToMeetGithubRequirements", () => {
-  it("truncates the string to less than 140 characters", async () => {
-    const testString = "x".repeat(300);
+describe('truncateDescriptionToMeetGithubRequirements', () => {
+  it('truncates the string to less than 140 characters', async () => {
+    const testString = 'x'.repeat(300)
     expect(
       truncateDescriptionToMeetGithubRequirements(testString).length
-    ).toBeLessThanOrEqual(140);
-  });
-});
+    ).toBeLessThanOrEqual(140)
+  })
+})
 
-describe("joinWithWhitelist", () => {
-  it("keeps only wanted whitelisted items", async () => {
-    const whitelist = ["keep me", "keep me too ignoring whitespace"];
+describe('joinWithWhitelist', () => {
+  it('keeps only wanted whitelisted items', async () => {
+    const whitelist = ['keep me', 'keep me too ignoring whitespace']
     const markdown = [
-      "- [ ] keep me",
-      "- [x]     keep me too ignoring whitespace      ",
-      "- [ ] ignore me"
-    ].join("\n");
-    const checklistItems = parseMarkdownChecklistItems(markdown);
+      '- [ ] keep me',
+      '- [x]     keep me too ignoring whitespace      ',
+      '- [ ] ignore me'
+    ].join('\n')
+    const checklistItems = parseMarkdownChecklistItems(markdown)
     expect(joinWithWhitelist(checklistItems, whitelist)).toEqual([
       {
-        description: "keep me",
+        description: 'keep me',
         checked: false
       },
       {
-        description: "keep me too ignoring whitespace",
+        description: 'keep me too ignoring whitespace',
         checked: true
       }
-    ]);
-  });
+    ])
+  })
 
-  it("adds whitelist items that are missing and set to true", async () => {
-    const whitelist = ["whitelist item 1", "missing whitelist item"];
+  it('adds whitelist items that are missing and set to true', async () => {
+    const whitelist = ['whitelist item 1', 'missing whitelist item']
     const markdown = [
-      "- [ ] whitelist item 1",
-      "- [ ] not a whitelist item"
-    ].join("\n");
-    const checklistItems = parseMarkdownChecklistItems(markdown);
+      '- [ ] whitelist item 1',
+      '- [ ] not a whitelist item'
+    ].join('\n')
+    const checklistItems = parseMarkdownChecklistItems(markdown)
     expect(joinWithWhitelist(checklistItems, whitelist)).toEqual([
       {
-        description: "whitelist item 1",
+        description: 'whitelist item 1',
         checked: false
       },
       {
-        description: "missing whitelist item",
+        description: 'missing whitelist item',
         checked: true
       }
-    ]);
-  });
-});
+    ])
+  })
+})
 
-describe("getGithubCheckSpecs", () => {
-  it("returns the correct specs for each item", async () => {
+describe('getGithubCheckSpecs', () => {
+  it('returns the correct specs for each item', async () => {
     const checklistItems = [
-      { description: "item 1", checked: true },
-      { description: "item 2", checked: false },
-      { description: "item 3", checked: false }
-    ];
+      {description: 'item 1', checked: true},
+      {description: 'item 2', checked: false},
+      {description: 'item 3', checked: false}
+    ]
     expect(getGithubStatusSpecs(checklistItems)).toEqual([
       {
-        description: "item 1",
+        description: 'item 1',
         success: true,
         id: 1
       },
       {
-        description: "item 2",
+        description: 'item 2',
         success: false,
         id: 2
       },
       {
-        description: "item 3",
+        description: 'item 3',
         success: false,
         id: 3
       }
-    ]);
-  });
-});
+    ])
+  })
+})
 
-describe("findChecklistItem", () => {
-  it("returns checklist item if it exists", async () => {
+describe('findChecklistItem', () => {
+  it('returns checklist item if it exists', async () => {
     const checklistItems = [
-      { description: "item 1", checked: false },
-      { description: "item 2", checked: false },
-      { description: "item 3", checked: true }
-    ];
-    expect(findChecklistItem(checklistItems, "item 1")).toEqual({
-      description: "item 1",
+      {description: 'item 1', checked: false},
+      {description: 'item 2', checked: false},
+      {description: 'item 3', checked: true}
+    ]
+    expect(findChecklistItem(checklistItems, 'item 1')).toEqual({
+      description: 'item 1',
       checked: false
-    });
-  });
-});
+    })
+  })
+})
 
-describe("makeWhitelistFromArgs", () => {
-  it("makes a list of checklist item strings from supplied Github action args", async () => {
+describe('makeWhitelistFromArgs', () => {
+  it('makes a list of checklist item strings from supplied Github action args', async () => {
     const args = {
-      repoToken: "acbdefg",
-      githubStatusContext: "",
-      checklistItem1: "item 1",
-      checklistItem2: "item 2",
-      checklistItem3: "item 3",
-      checklistItem4: "",
-      checklistItem5: ""
-    };
-    expect(makeWhitelistFromArgs(args)).toEqual(["item 1", "item 2", "item 3"]);
-  });
-});
+      repoToken: 'acbdefg',
+      githubStatusContext: '',
+      checklistItem1: 'item 1',
+      checklistItem2: 'item 2',
+      checklistItem3: 'item 3',
+      checklistItem4: '',
+      checklistItem5: ''
+    }
+    expect(makeWhitelistFromArgs(args)).toEqual(['item 1', 'item 2', 'item 3'])
+  })
+})
